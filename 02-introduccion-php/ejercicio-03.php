0afa8167aa29dca954d36a4d5003d4a6c809c7ba
Brandon <brandonss0604@gmail.com>
2020-07-11 15:38:54 -0430

    02-introduccion-php

---
+++ 02-introduccion-php/ejercicio-03.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sitio Web con PHP</title>
+</head>
+<body>
+	<?php 
+		$nombre= "<b>Brandon Silva</b>";
+
+		echo "Hola gente " . $nombre;
+	 ?>
+
+
+</body>
+</html>
