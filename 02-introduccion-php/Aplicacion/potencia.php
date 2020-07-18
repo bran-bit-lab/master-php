2c25d50cc935e44e860baa71e8eb9e30741ad45a
gabriel <gabmat1995@gmail.com>
2020-07-18 16:39:03 -0400

    Ejercicio de Potencia

---
+++ 02-introduccion-php/Aplicacion/potencia.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+$contador= 1;
+
+echo "n \t10 \t100 \t10000 \n";
+
+while ($contador <= 5) {
+	
+
+	echo $contador ."\t". ($contador *10) ."\t". ($contador *100) ."\t". ($contador *1000) ."\n";
+	$contador++;
+}
+
+
+ ?>
