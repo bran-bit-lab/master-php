79cf04dfb27a9eddee5e2076fe8b33bbb5f4ab66
gabriel <gabmat1995@gmail.com>
2020-08-23 10:22:11 -0400

    wip: modificaciones finales a la calculadora bienaria version 1.0

--- 03-sintaxis-basica-php/binario.php
+++ 03-sintaxis-basica-php/binario.php
@@ -1,29 +1,41 @@
 <?php  
 
 	// inclusion de archivos a codigo php, otra opcion para llamar el archivo es usando require
+
 	include "./array_ascii.php";  
 
 	// Funciones de string a binario
 
 	function separarLetras( $palabra= "" ){
-		
-		// echo strlen( $palabra ); Para obtener longitud del texto
-		// echo substr( $palabra, 1, 1 ); Para obtener un valor especifico del texto
-		
-		for ($contador=0; $contador < strlen( $palabra ); $contador++) { 
+
+		$patron= '/^[A-Za-z]+$/';
+
+		if ( preg_match( $patron, $palabra ) > 0 ) {
+
+			// echo strlen( $palabra ); Para obtener longitud del texto
+			// echo substr( $palabra, 1, 1 ); Para obtener un valor especifico del texto
 			
-			// echo substr($palabra, $contador, 1) . "\n"; Imprimir letra especifico del texto
+			for ($contador=0; $contador < strlen( $palabra ); $contador++) { 
+				
+				// echo substr($palabra, $contador, 1) . "\n"; Imprimir letra especifico del texto
+				
+				$letra= substr( $palabra, $contador, 1 );
+				conversionBinaria( $letra );
+			}
 			
-			$letra= substr($palabra, $contador, 1);
-			conversionBinaria($letra);
+			// conversionBinaria("e");
+		
+		} else {
+
+			echo "El valor introducido no es una palabra\n";
 		}
 		
-		// conversionBinaria("e");
 	}
 
 	function conversionBinaria( $letra ){
 
 		// global para usar varaiable del array ascii dentro de la funcion
+
 		global $arrayAscii;
 		global $binario;
 		
@@ -40,6 +52,7 @@
 				}
 
 				// bucle inverso para obtener el binario
+
 				$contador = strlen( $string ) - 1; 
 
 				while ($contador >= 0){
@@ -53,6 +66,7 @@
 				}
 				
 				// sale del bucle
+
 				break; 
 			}
 		}
@@ -61,27 +75,29 @@
 
 	// Funciones de binario a string
 
-	function binarioaString($valorBinario){
+	function binarioaDecimal($valorBinario){
 
 		$exponente= 0;
 		$valor= 0;
 
-		for ($contador= strlen($valorBinario)-1; $contador >= 0; $contador--) { 
+		for ($contador= strlen( $valorBinario ) -1; $contador >= 0; $contador--) { 
+			
 			$digito = (int) substr($valorBinario, $contador, 1);
 
 			if ($digito == 1) {
 				$valor= $valor + ( pow(2, $exponente) );
 			}
+			
 			$exponente++;	
 		}
-		return $valor;
-
 		
+		return $valor;
 	}
 
-	function validarBinario($valorBinario= ""){
+	function separarBinario( $valorBinario= "" ){
 		
 		global $arrayAscii;
+		global $palabra;
 
 		// Variable en la que se definen expresiones regulares
 		$patron = "/^[0-1]+$/";
@@ -94,55 +110,68 @@
 				
 				$seccionBinaria= substr($valorBinario, $contador, 7);
 
-				$decimal= binarioaString($seccionBinaria);
+				$decimal= binarioaDecimal($seccionBinaria);
 
 				foreach ( $arrayAscii as $clave => $valor) {
 					
 					if ($decimal == $valor) {
 						
-						echo "$clave" . " ";
-					
+						$palabra.= $clave;
 						break;
-				}
-
-				echo "\n";
-			}
-
-			
-
-			
+					}
+				}	
 			}
 
 		} else {
-    		echo "El valor no es binario\n";
+    		echo "El valor no es binario válido\n";
 
 		}
 	}
 
+	// Main.php
 
-	// ejecucion automatica
+	$app = true; // valor bucle centinela
+
 	echo "Bienvenido a la calculadora binaria \n";
 
-	echo "¿Que desea convertir? \n";
-	echo "1.- Convertir a binario \n";
-	echo "2.- Convertir de binario a caracter \n";
+	do {
 
-	$opcion= (int) readline("Ingrese una opcion: ");
+		echo "¿Que desea convertir? \n";
+		echo "1.- Convertir a binario \n";
+		echo "2.- Convertir de binario a caracter \n";
+		echo "3.- Salir \n\n";
 
-	$binario= "";
+		// se setean los valores
+		$binario= "";
+		$option= 0;
+		$palabra = "";
+
+		$opcion= (int) readline("Ingrese el número de la opcion: ");
+
+		switch ($opcion) {
+
+		    case 1:
+		    	$palabra= readline("Ingrese la palabra: ");
+		        separarLetras( $palabra );
+		        echo "el binario generado es: $binario \n\n";
+		        break;
+
+		    case 2:
+		    	$valorBinario= readline("Ingrese el codigo binario: ");
+		    	separarBinario( $valorBinario );
+		    	echo "La palabra generada es: $palabra \n\n";
+		        break;
+
+		    case 3:
+		    	// cierre del app.
+		    	$app = false;
+		    	break;
+
+	    	default:
+	    		echo "La opcion no es valida\n\n";
+				break;
+		}	
+
+	} while ( $app );
 	
-	switch ($opcion) {
-	    case 1:
-	    	$palabra= readline("Ingrese la palabra: ");
-	        separarLetras($palabra);
-	        echo "$binario\n";
-	        break;
-	    case 2:
-	    	$valorBinario= readline("Ingrese el codigo binario: ");
-	    	validarBinario($valorBinario);
-	        break;
-    	default:
-    		echo "La opcion no es valida";
-			break;
-	}	
 ?>
